39752ae8ba938aa7a7b01ad7f07895b37d32c2d4
azeugb <55405445+azeugb@users.noreply.github.com>
2021-03-21 12:49:02 +0100

    Add forward run mechanisms

--- Assets/CustomScripts/GameManager.cs
+++ Assets/CustomScripts/GameManager.cs
@@ -8,13 +8,30 @@ namespace UnityEngine.XR.ARFoundation
     {
         public const int windowSize = 50;
         public const float multiplier = 20;
-        public const float alpha = 0.2f;
+        public const float alpha = 0.4f;
+        public const float trackLength = 100f;
+        public const float maxVelocity = 10f;
+        public const float jumpPower = 150f;
+        public const float spawnDistanceMin = 100;
+        public const float spawnDistanceMax = 200f;
+        public const float safeDistance = 10f;
 
         public GameObject player = null;
         public TextMesh gameText = null;
 
+        public GameObject[] tracks;
+        public List<GameObject> obstacles;
+        public List<GameObject> instiatedObstacles;
+
+        GameObject obst;
+        Rigidbody playerRigidBody;
+
         int frameCount = 0;
         int arrayCount = 0;
+        int trackIndex = 0;
+
+        float trackDistance = trackLength;
+        float spawnDistance = 0;
 
         float facePositionY = 0;
         float min = 0;
@@ -22,20 +39,21 @@ namespace UnityEngine.XR.ARFoundation
         float prevVelocity = 0;
         float currVelocity = 0;
         float average = 0;
-        float stepCount = 0;
         List<float> facePositions;
 
         float startTime = 0;
         float interval = 0;
+        IEnumerator InstantiateObstacleCoroutine;
 
         // Start is called before the first frame update
         void Start()
         {
             facePositions = new List<float>();
+            playerRigidBody = player.GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
-        void FixedUpdate()
+        void Update()
         {
             facePositionY = StaticVariable.positionY;
             facePositions.Add(facePositionY);
@@ -55,7 +73,6 @@ namespace UnityEngine.XR.ARFoundation
 
                 float intervalMultiplier = interval > 0 ? multiplier / interval : multiplier;
                 currVelocity = Mathf.Abs(max - min) * intervalMultiplier;
-
             }
 
             arrayCount = facePositions.Count;
@@ -66,13 +83,50 @@ namespace UnityEngine.XR.ARFoundation
                 startTime = Time.time;
             }
 
-            currVelocity = Mathf.Lerp(prevVelocity, currVelocity, alpha);
-            gameText.text = currVelocity.ToString();
+            //currVelocity = 10f; //////////////////////////////////////////// - For Unity Play Mode - //////////////////////////////////////////////////////
+
+            currVelocity = currVelocity > maxVelocity ? maxVelocity : currVelocity;
 
             player.transform.localPosition += Vector3.forward * Time.deltaTime * currVelocity;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerRigidBody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+
+            if (player.transform.localPosition.z >= trackDistance)
+            {
+                trackDistance += trackLength;
+                Vector3 trackPosition = tracks[trackIndex].transform.localPosition;
+                tracks[trackIndex].transform.localPosition = new Vector3(trackPosition.x, trackPosition.y, trackPosition.z + trackLength * (tracks.Length));
+
+                trackIndex++;
+                trackIndex %= tracks.Length;
+            }
+
+            if (player.transform.localPosition.z > spawnDistance)
+            {
+                spawnDistance = player.transform.localPosition.z + Random.Range(spawnDistanceMin, spawnDistanceMax);
+                InstantiateObstacleCoroutine = InstantiateObstacle(spawnDistance);
+                StartCoroutine(InstantiateObstacleCoroutine);
+            }
+
             prevVelocity = currVelocity;
 
             frameCount++;
+        }
+        IEnumerator InstantiateObstacle(float spawnDistance)
+        {
+            foreach (GameObject obj in instiatedObstacles)
+            {
+                Destroy(obj);
+            }
+            instiatedObstacles.Clear();
+            obst = Instantiate(obstacles[Random.Range(0, obstacles.Count)], new Vector3(0f, 0.01f, spawnDistance), Quaternion.identity) as GameObject;
+            //obst = Instantiate(obstacles[14], new Vector3(0f, 0.01f, spawnDistance), Quaternion.identity) as GameObject;
+            instiatedObstacles.Add(obst);
+
+            yield return new WaitForEndOfFrame();
         }
     }
 }
